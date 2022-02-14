0308d3f9aad3a26ee0cf3d87b7028defc74b5988
PatriziaCrimi <crimi.patrizia@gmail.com>
2022-02-14 14:13:07 +0100

    HomeController, DressController & Dress model added

--- app/Dress.php
+++ app/Dress.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Dress extends Model
 {
-  protected $fillable = ['name', 'color', 'size', 'description', 'price', 'season'];
+  protected $fillable['name', 'color', 'size', 'description', 'price', 'season'];
 }
